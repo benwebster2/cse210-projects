55ffe0326651750964439ed901253c5a60ac8fb2
Ben Webster <benjamminproductions28@gmail.com>
2026-05-15 17:45:08 -0600

    more changes.

--- journal/program.cs
+++ journal/program.cs
@@ -8,29 +8,26 @@ class Program
         PromptGenerator promptGen = new PromptGenerator();
         string choice = "";
 
-        Console.WriteLine("What's up? Start writing in your journal!");
-
         while (choice != "5")
         {
-            Console.WriteLine("\nPlease select one of the following choices:");
+            Console.WriteLine("Please select one of the following choices:");
             Console.WriteLine("1. Write");
             Console.WriteLine("2. Display");
             Console.WriteLine("3. Load");
             Console.WriteLine("4. Save");
             Console.WriteLine("5. Quit");
-            Console.Write("What do you want to do? ");
+            Console.Write("What would you like to do? "); 
             choice = Console.ReadLine();
 
             if (choice == "1") 
             {
                 string prompt = promptGen.GetRandomPrompt();
-                Console.WriteLine($"\n{prompt}");
-                Console.Write("> ");
+                Console.WriteLine(prompt);
+                Console.Write("> "); 
                 string response = Console.ReadLine();
 
                 Entry newEntry = new Entry();
                 newEntry._date = DateTime.Now.ToShortDateString();
-                
                 newEntry._prompt = prompt;
                 newEntry._entry = response;
 
@@ -38,23 +35,22 @@ class Program
             }
             else if (choice == "2") 
             {
-                Console.WriteLine();
-                theJournal.DisplayAll();
+                theJournal.DisplayEntries(); 
             }
             else if (choice == "3") 
             {
-                Console.Write("What is the filename? ");
+                Console.WriteLine("What is the filename?");
+                Console.Write(" "); 
                 string filename = Console.ReadLine();
                 theJournal.LoadFromFile(filename);
             }
             else if (choice == "4") 
             {
-                Console.Write("What is the filename? ");
+                Console.WriteLine("What is the filename?");
+                Console.Write(" ");
                 string filename = Console.ReadLine();
                 theJournal.SaveToFile(filename);
             }
         }
-
-        Console.WriteLine("Goodbye!");
     }
 }
